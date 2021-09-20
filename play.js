0d7734b6257e85d5d0ef873f100abc0ec0529051
Adriana <adriana.calvomatos@gmail.com>
2021-09-20 13:43:52 +0000

    refactor play to import connect

---
+++ play.js
@@ -0,0 +1,7 @@
+//  main file through which you will launch the game client.
+
+// sets up a connection and returns an object
+const { connect } = require('./client');
+
+console.log('Connecting ...');
+connect();
