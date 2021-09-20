77f3f5887d1523a1c924e4310bf3080177935388
Adriana <adriana.calvomatos@gmail.com>
2021-09-20 14:27:33 +0000

    send username to server upon connecting

--- client.js
+++ client.js
@@ -2,6 +2,8 @@
 const net = require('net');
 
 const connect = function () {
+  const name = 'yzz';
+
   // this conn obj represents the connection that you have with the server.
   const conn = net.createConnection({
     host: '135.23.223.133', // IP address here,
@@ -11,8 +13,13 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
+  conn.on('data', (data) => {
+    console.log('Received: ' + data);
+  });
+
   conn.on('connect', () => {
-    console.log('You are connected!');
+    console.log('You are connected to the game server!');
+    conn.write(`Name: ${name}`);
   });
 
   return conn;
